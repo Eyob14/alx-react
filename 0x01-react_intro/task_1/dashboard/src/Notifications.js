a1da9bd42d93bfdf8f5a6173d9f2dcafe1319e2c
Eyob14 <eyobzebene3580@gmail.com>
2022-08-25 01:35:33 +0300

    task_1/

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Notifications.css";
+
+class NotificationSlide extends Component {
+  render() {
+    return (
+      <div className="Notification">
+        <p>Here is the list of notifications</p>
+      </div>
+    );
+  }
+}
+
+export default NotificationSlide;
